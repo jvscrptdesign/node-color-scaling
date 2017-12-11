47a4dd9f698caa9f2839745941dc8c09858d8371
István Blastik <blastik86@gmail.com>
2017-12-11 21:11:02 +0100

    Update InputBox_.jsx

--- src/components/InputBox_.jsx
+++ src/components/InputBox_.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import '../styles/inputBox_.css';
 
 class InputBox extends Component {
@@ -10,12 +11,27 @@ class InputBox extends Component {
                     type="number" 
                     min="0"
                     max={this.props.max}
-                    name={this.props.type}
-                    id={this.props.type}
+                    row={this.props.row}
+                    defaultValue={this.props.defaultValue}
+                    onChange={this.props.onChange}
+                    //TODO: too many properties with the same value -> choose one
+                    hsltype={this.props.hsltype}
+                    name={this.props.hsltype}
+                    id={this.props.hsltype}
                     className="inputBox"/>
             </div>
         );
     }
+}
+
+InputBox.propTypes = {
+    max: PropTypes.number.isRequired,
+    row: PropTypes.oneOf(['H', 'S', 'L']).isRequired,
+    defaultValue: PropTypes.string.isRequired, // :S
+
+    name: PropTypes.string.isRequired,
+    id: PropTypes.oneOf(['H1', 'H2', 'S1', 'S2', 'L1', 'L2']).isRequired,
+    hsltype: PropTypes.oneOf(['H1', 'H2', 'S1', 'S2', 'L1', 'L2']).isRequired
 }
 
 export default InputBox;
