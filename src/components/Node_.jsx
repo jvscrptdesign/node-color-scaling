894a9652479da3f6350563e5791686cdba169d4a
István Blastik <blastik86@gmail.com>
2017-12-13 17:17:28 +0100

    Update Node_.jsx

--- src/components/Node_.jsx
+++ src/components/Node_.jsx
@@ -1,16 +1,24 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import '../styles/node_.css';
 
 class Node extends Component {
     render() {
+        let style = {
+            backgroundColor: `hsl(${this.props.h}, ${this.props.s}%, ${this.props.l}%)`
+        }
         return (
             <div 
                 className="node row" 
-                nodeh={this.props.nodeh} 
-                nodes={this.props.nodes} 
-                nodel={this.props.nodel}></div>
+                style={style}></div>
         );
     }
+}
+
+Node.propTypes = {
+    h: PropTypes.number.isRequired,
+    s: PropTypes.number.isRequired,
+    l: PropTypes.number.isRequired,
 }
 
 export default Node;
