11e71e5347becbd650d35860308ab1f0a01b4bb7
István Blastik <blastik86@gmail.com>
2017-12-13 18:36:00 +0100

    Update NodeLine_.jsx

--- src/components/NodeLine_.jsx
+++ src/components/NodeLine_.jsx
@@ -4,53 +4,66 @@ import PropTypes from 'prop-types';
 import '../styles/nodeLine_.css';
 
 class NodeLine extends Component {
-    render() {
+    constructor(props) {
+        super(props);
+        this.state = {
+            numOfNodes: 14
+        }
+        this.getH = this.getH.bind(this);
+        this.getS = this.getS.bind(this);
+        this.getL = this.getL.bind(this);
+        this.buildNodeLine = this.buildNodeLine.bind(this);
+    }
+
+    getH(i) {
         let H1 = this.props.H1,
             H2 = this.props.H2,
-            S1 = this.props.S1,
+            hRange = H2 > H1 ? H2 - H1 : H1 - H2,
+            hAvg = hRange / this.state.numOfNodes;
+
+        return H1 + hAvg * i;
+    }
+
+    getS(i) {
+        let S1 = this.props.S1,
             S2 = this.props.S2,
-            L1 = this.props.L1,
+            sRange = S2 > S1 ? S2 - S1 : S1 - S2,
+            sAvg = sRange / this.state.numOfNodes;
+
+        return S1 + sAvg * i;
+    }
+
+    getL(i) {
+        let L1 = this.props.L1,
             L2 = this.props.L2,
+            lRange = L2 > L1 ? L2 - L1 : L1 - L2,
+            lAvg = lRange / this.state.numOfNodes;
+
+        return L1 + lAvg * i;
+    }
+
+    buildNodeLine() {
+        let H1 = this.props.H1,
+            S1 = this.props.S1,
+            L1 = this.props.L1,
             aHSL = this.props.activehsl;
 
-        let hRange = H2 > H1 ? H2 - H1 : H1 - H2,
-            hAvg = hRange / 13,
-            sRange = S2 > S1 ? S2 - S1 : S1 - S2,
-            sAvg = sRange / 13,
-            lRange = L2 > L1 ? L2 - L1 : L1 - L2,
-            lAvg = lRange / 13;
+        var nodes = [];
+        for (var i = 0; i <= this.state.numOfNodes; i++) {
+            nodes.push(<Node 
+                h={aHSL==='H' ? this.getH(i) : H1} 
+                s={aHSL==='S' ? this.getS(i) : S1} 
+                l={aHSL==='L' ? this.getL(i) : L1} key={i} />);
+        }
+        return nodes;
+    }
 
-        //componentWillUpdate (nextProps, nextState)
-        
-        function getH(i) {
-            return H1 + hAvg * i;
-        }
-        function getS(i) {
-            return S1 + sAvg * i;
-        }
-        function getL(i) {
-            return L1 + lAvg * i;
-        }
+    //componentWillUpdate (nextProps, nextState)
 
+    render() {
         return (
-            <div className="nodeLine">
-                <div>
-                    {/*map*/}
-                    <Node h={aHSL==='H' ? getH(0) : H1} s={aHSL==='S' ? getS(0) : S1} l={aHSL==='L' ? getL(0) : L1}/>
-                    <Node h={aHSL==='H' ? getH(1) : H1} s={aHSL==='S' ? getS(1) : S1} l={aHSL==='L' ? getL(1) : L1}/>
-                    <Node h={aHSL==='H' ? getH(2) : H1} s={aHSL==='S' ? getS(2) : S1} l={aHSL==='L' ? getL(2) : L1}/>
-                    <Node h={aHSL==='H' ? getH(3) : H1} s={aHSL==='S' ? getS(3) : S1} l={aHSL==='L' ? getL(3) : L1}/>
-                    <Node h={aHSL==='H' ? getH(4) : H1} s={aHSL==='S' ? getS(4) : S1} l={aHSL==='L' ? getL(4) : L1}/>
-                    <Node h={aHSL==='H' ? getH(5) : H1} s={aHSL==='S' ? getS(5) : S1} l={aHSL==='L' ? getL(5) : L1}/>
-                    <Node h={aHSL==='H' ? getH(6) : H1} s={aHSL==='S' ? getS(6) : S1} l={aHSL==='L' ? getL(6) : L1}/>
-                    <Node h={aHSL==='H' ? getH(7) : H1} s={aHSL==='S' ? getS(7) : S1} l={aHSL==='L' ? getL(7) : L1}/>
-                    <Node h={aHSL==='H' ? getH(8) : H1} s={aHSL==='S' ? getS(8) : S1} l={aHSL==='L' ? getL(8) : L1}/>
-                    <Node h={aHSL==='H' ? getH(9) : H1} s={aHSL==='S' ? getS(9) : S1} l={aHSL==='L' ? getL(9) : L1}/>
-                    <Node h={aHSL==='H' ? getH(10) : H1} s={aHSL==='S' ? getS(10) : S1} l={aHSL==='L' ? getL(10) : L1}/>
-                    <Node h={aHSL==='H' ? getH(11) : H1} s={aHSL==='S' ? getS(11) : S1} l={aHSL==='L' ? getL(11) : L1}/>
-                    <Node h={aHSL==='H' ? getH(12) : H1} s={aHSL==='S' ? getS(12) : S1} l={aHSL==='L' ? getL(12) : L1}/>
-                    <Node h={aHSL==='H' ? getH(13) : H1} s={aHSL==='S' ? getS(13) : S1} l={aHSL==='L' ? getL(13) : L1}/>
-                </div>
+            <div className="nodeLine" id="nodeLine">
+                {this.buildNodeLine()}
             </div>
         );
     }
