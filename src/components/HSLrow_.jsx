e01be34f0c37c6fed7426600a0ce9a001be9a0f6
István Blastik <blastik86@gmail.com>
2017-12-16 13:20:02 +0100

    Update HSLrow_.jsx

--- src/components/HSLrow_.jsx
+++ src/components/HSLrow_.jsx
@@ -2,42 +2,57 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import ColorBar from './ColorBar_.jsx';
 import InputBox from './InputBox_.jsx';
+import Pointer from './Pointer_.jsx';
 
 class HSLrow extends Component {
     render() {
         let inputSpace = {
-            width:'75px',
-            height:'67px'    
+            width:'75px'   
+        }
+        let label = {
+            height:'25px',
+            paddingTop:'2px'
+        }
+        let shrinkRow = {
+            height: '26px'
         }
 
         return (
-            <div className="row" row={this.props.row}>
-                <div className="colorSectionLabel">{this.props.row}&nbsp;&nbsp;</div>
-                <InputBox 
-                    row={this.props.row}
-                    defaultValue={this.props.HSL1}
-                    onChange={this.props.onChangeHSL1}
-                    //TODO: too many properties with the same value -> choose one
-                    name={`${this.props.row}1`}
-                    id={`${this.props.row}1`}/>
+            <div>
+                <div className="pointerRow">
+                    <Pointer pType="down" HSL={this.props.HSL1}/>
+                </div>
 
-                <ColorBar
-                    row={this.props.row}
-                    activeHval={this.props.activeHval}
-                    activehsl={this.props.activehsl}
-                    HSL1={this.props.HSL1}
-                    HSL2={this.props.HSL2}
-                />
-                
-                {this.props.row===this.props.activehsl ? 
+                <div className="row" row={this.props.row} style={shrinkRow}>
+                    <div style={label}>{this.props.row}&nbsp;&nbsp;</div>
+                    <InputBox 
+                        row={this.props.row}
+                        defaultValue={this.props.HSL1}
+                        onChange={this.props.onChangeHSL1}
+                        //TODO: too many properties with the same value -> choose one
+                        name={`${this.props.row}1`}
+                        id={`${this.props.row}1`}/>
+
+                    <ColorBar
+                        row={this.props.row}
+                        activeHval={this.props.activeHval}
+                        activehsl={this.props.activehsl}
+                        HSL1={this.props.HSL1}
+                        HSL2={this.props.HSL2}/>
+                    
                     <InputBox 
                         row={this.props.row}
                         defaultValue={this.props.HSL2}
                         onChange={this.props.onChangeHSL2}
                         //TODO: too many properties with the same value -> choose one
                         name={`${this.props.row}2`}
-                        id={`${this.props.row}2`}/> : 
-                    <div style={ inputSpace }></div>}
+                        id={`${this.props.row}2`}
+                        visibility={this.props.row===this.props.activehsl ? "" : "hidden"}/>
+                </div>
+
+                <div className="pointerRow">
+                    <Pointer pType="up" HSL={this.props.HSL2} visibility={this.props.row===this.props.activehsl ? "" : "hidden"}/>
+                </div>
             </div>
         );
     }
