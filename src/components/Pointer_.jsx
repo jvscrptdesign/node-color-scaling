85b171c42bd1d738c4a9df373077084eb86cf943
István Blastik <blastik86@gmail.com>
2019-04-22 11:11:15 +0200

    pointer as stateless functional component

--- src/components/Pointer_.jsx
+++ src/components/Pointer_.jsx
@@ -1,21 +1,19 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import '../styles/pointer_.css';
 
-class Pointer extends Component {
-    render() {
-        let {row, pType, visibility, HSL} = this.props;
-        let left = (row==='H') ? `${HSL}px` : `${HSL*3.6}px`;
+const Pointer = ({row, pType, visibility, HSL}) => {
+    let left = (row==='H') ? `${HSL}px` : `${HSL*3.6}px`;
 
-        return (
-            <div 
-                className={`pointer${pType}`}
-                type={pType}
-                style={{visibility: `${visibility}`}}>
-                <div style={{left: left}}>{HSL}<i className={`fa-caret-${pType}`} style={{visibility: `${visibility}`}}/></div>
-            </div>
+    return (
+        <div 
+            className={`pointer${pType}`}
+            type={pType}
+            style={{visibility: `${visibility}`}}>
+			
+            <div style={{left: left}}>{HSL}<i className={`fa-caret-${pType}`} style={{visibility: `${visibility}`}}/></div>
+         </div>
         );
-    }
 }
 
 Pointer.propTypes = {
